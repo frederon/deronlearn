c9dc6e481e8d7683d53472b61fb823fee57aceb7
Frederic Ronaldi <frederic.ronaldi@gmail.com>
2019-12-09 09:03:11 +0700

    Added default quality and warning for not loading the video

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,6 @@
-const player = new Plyr("#player", {});
+const player = new Plyr("#player", {
+  quality: {
+    default: 1080,
+    options: [1440, 1080, 720]
+  }
+});
--- routes/videos.js
+++ routes/videos.js
@@ -2,9 +2,9 @@ var express = require("express");
 var router = express.Router();
 var fs = require("fs");
 
-router.get("/:courseId/:videoId", function(req, res, next) {
+router.get("/:courseId/:videoId/:size", function(req, res, next) {
   if (req.isAuthenticated()) {
-    const path = `video/${req.params.courseId}/${req.params.videoId}.mp4`;
+    const path = `video/${req.params.courseId}/${req.params.videoId}/${req.params.size}.mp4`;
     const stat = fs.statSync(path);
     const fileSize = stat.size;
     const range = req.headers.range;
